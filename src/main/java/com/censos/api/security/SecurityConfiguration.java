108444b8d3a8279599c00061f2b87d14e41b2cf5
Lukas A. Santiago <lukas.asant@gmail.com>
2022-04-25 13:59:30 -0300

    bcrypt implementation

--- src/main/java/com/censos/api/security/SecurityConfiguration.java
+++ src/main/java/com/censos/api/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.censos.api;
+package com.censos.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,12 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -25,26 +33,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     @Bean
     public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         UserDetails user = User
                 .withUsername("user")
-                .password(encoder.encode("user"))
+                .password(passwordEncoder.encode("user"))
                 .roles("USER").build();
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(user);
         return manager;
     }
-
-    // @Autowired
-    // @Qualifier("datasource")
-    // private DataSource dataSource;
-
-    // @Autowired
-    // public void configureGlobal(AuthenticationManagerBuilder auth) throws
-    // Exception {
-    // auth.jdbcAuthentication().dataSource(dataSource)
-    // .authoritiesByUsernameQuery("select email, role FROM user where email=?")
-    // .usersByUsernameQuery("select email,password,active FROM employee where
-    // email=?");
-    // }
 }
