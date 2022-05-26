e8dc79851c781c58d30bfc046d36c35b4b526fb4
lukas-santiago <lukas.asant@gmail.com>
2022-05-26 19:33:23 -0300

    funcionando

--- src/main/java/com/censos/api/repository/FormExecutionRepository.java
+++ src/main/java/com/censos/api/repository/FormExecutionRepository.java
@@ -6,9 +6,14 @@ import java.util.Optional;
 import com.censos.api.entity.FormExecution;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FormExecutionRepository extends JpaRepository<FormExecution, Long> {
 
     Collection<FormExecution> findAllByFormId(Long formId);
     Optional<FormExecution> findAllByFormFieldId(Long formFieldId);
+
+    
+    @Query(value = "SELECT * FROM form_execution f WHERE f.created_by = ?1 AND f.form_id = ?2", nativeQuery = true)
+    Collection<FormExecution> findAllByUserAndByFormId(String createdBy, Long formId);
 }
