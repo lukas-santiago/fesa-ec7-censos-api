3d7abdc3c9af81f9e56d2bb609b1ce05a9e8a548
lukas-santiago <lukas.asant@gmail.com>
2022-05-26 21:39:40 -0300

    ajuste

--- docs/create-schema.sql
+++ docs/create-schema.sql
@@ -1,67 +1,75 @@
 CREATE DATABASE IF NOT EXISTS `censos`
 
 USE `censos`
+CREATE TABLE `field` (
+  `field_id` bigint NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`field_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `user`
-(
- `user_id`  bigint NOT NULL ,
- `username` varchar(45) NOT NULL ,
- `email`    varchar(45) NOT NULL ,
- `password` varchar(45) NOT NULL ,
+CREATE TABLE `form` (
+  `form_id` bigint NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `last_modified_by` varchar(255) DEFAULT NULL,
+  `last_modified_date` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `expired_at` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`form_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-PRIMARY KEY (`user_id`)
-);
+CREATE TABLE `form_execution` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `answer` varchar(255) DEFAULT NULL,
+  `form_field_id` bigint DEFAULT NULL,
+  `form_id` bigint DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `last_modified_by` varchar(255) DEFAULT NULL,
+  `last_modified_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `form`
-(
- `form_id`     bigint NOT NULL ,
- `code`        varchar(45) NOT NULL ,
- `name`        varchar(45) NOT NULL ,
- `description` varchar(200) NOT NULL ,
- `expired_at`  datetime NOT NULL ,
- `user_id`     bigint NOT NULL ,
+CREATE TABLE `form_field` (
+  `form_field_id` bigint NOT NULL AUTO_INCREMENT,
+  `data` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `field_id` bigint DEFAULT NULL,
+  `form_id` bigint DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `last_modified_by` varchar(255) DEFAULT NULL,
+  `last_modified_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`form_field_id`),
+  KEY `FKbmfw9o6lhfphgei7m249xeb0t` (`field_id`),
+  KEY `FKbgdx9knhcvbjyxlq9iv0x04es` (`form_id`),
+  CONSTRAINT `FKbgdx9knhcvbjyxlq9iv0x04es` FOREIGN KEY (`form_id`) REFERENCES `form` (`form_id`),
+  CONSTRAINT `FKbmfw9o6lhfphgei7m249xeb0t` FOREIGN KEY (`field_id`) REFERENCES `field` (`field_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-PRIMARY KEY (`form_id`),
-KEY `FK_35` (`user_id`),
-CONSTRAINT `FK_32` FOREIGN KEY `FK_35` (`user_id`) REFERENCES `user` (`user_id`)
-);
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `field`
-(
- `field_id` bigint NOT NULL ,
- `type`     varchar(45) NOT NULL ,
+CREATE TABLE `user_roles` (
+  `user_id` bigint NOT NULL,
+  `role_id` bigint NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FKh8ciramu9cc9q3qcqiv4ue8a6` (`role_id`),
+  CONSTRAINT `FKh8ciramu9cc9q3qcqiv4ue8a6` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `FKhfh9dx7w3ubf1co1vdev94g3f` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-PRIMARY KEY (`field_id`)
-);
-
-CREATE TABLE `form_field`
-(
- `form_field_id` bigint NOT NULL ,
- `form_id`       bigint NOT NULL ,
- `field_id`      bigint NOT NULL ,
- `description`   varchar(45) NOT NULL ,
- `data`          varchar(45) NOT NULL ,
-
-PRIMARY KEY (`form_field_id`),
-KEY `FK_40` (`field_id`),
-CONSTRAINT `FK_38` FOREIGN KEY `FK_40` (`field_id`) REFERENCES `field` (`field_id`),
-KEY `FK_43` (`form_id`),
-CONSTRAINT `FK_41` FOREIGN KEY `FK_43` (`form_id`) REFERENCES `form` (`form_id`)
-);
-
-CREATE TABLE `answer`
-(
- `answer_id`     bigint NOT NULL ,
- `answer`        varchar(45) NOT NULL ,
- `form_id`       bigint NOT NULL ,
- `form_field_id` bigint NOT NULL ,
- `user_id`       bigint NOT NULL ,
-
-PRIMARY KEY (`answer_id`),
-KEY `FK_50` (`user_id`),
-CONSTRAINT `FK_48` FOREIGN KEY `FK_50` (`user_id`) REFERENCES `user` (`user_id`),
-KEY `FK_53` (`form_field_id`),
-CONSTRAINT `FK_51` FOREIGN KEY `FK_53` (`form_field_id`) REFERENCES `form_field` (`form_field_id`),
-KEY `FK_59` (`form_id`),
-CONSTRAINT `FK_57` FOREIGN KEY `FK_59` (`form_id`) REFERENCES `form` (`form_id`)
-);
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKr43af9ap4edm43mmtq01oddj6` (`username`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
